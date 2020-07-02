059cf8dc8d4edc49d329c66c6526719dca63c93c
JonasRoo <jonas-roosen@t-online.de>
2020-07-02 15:42:16 +0200

    Documentation | default values | cleanup

--- json_utils.py
+++ json_utils.py
@@ -2,7 +2,7 @@ import json
 import glob
 import os
 
-def dump_json_to_file(data: dict, folder_name: str = ".", file_name: str = "data.json", **kwargs):
+def dump_json_to_file(data: dict, folder_name: str = ".", file_name: str = "data.json", **kwargs) -> None:
     """
     Produces a formatted .json file from a dictionary at a given path.
 
@@ -11,6 +11,9 @@ def dump_json_to_file(data: dict, folder_name: str = ".", file_name: str = "data
         - folder_name (optional): either `full path` or `relative` path
                        of the folder to save the file in (default: current directory)
         - file_name (optional): the name of new .json file (default: `data.json`)
+
+    @returns:
+        None
     """
     if not os.path.isdir(folder_name):
         raise ValueError(f"`{folder_name}` is not a valid directory!")
@@ -19,14 +22,22 @@ def dump_json_to_file(data: dict, folder_name: str = ".", file_name: str = "data
         json.dump(data, f, ensure_ascii=False, indent=4, **kwargs)
 
 
-def merge_all_json_in_folder(dir_name: str, merged_file_name: str = None) -> None:
+def merge_all_json_in_folder(dir_name: str, merged_file_name: str = "merged_data.json") -> None:
+    """
+    Merges all json files in a given directory into a single formatted .json file.
+
+    @args:
+        - dir_name: The full path or relative path of the directory
+                    containing the .json files to merge
+        - merged_file_name (optional): the name of the file containing the merged data
+                                       (default `merged_data.json`)
+    """
     data = []
     json_wildcard_path = os.path.join(dir_name, "*.json")
     for f in glob.glob(json_wildcard_path):
         with open(f, "r", encoding="utf-8") as input_file:
             data.append(json.load(input_file))
 
-    results_file_name = merged_file_name or "merged_data.json"
-    results_file_path = os.path.join(dir_name, results_file_name)
+    results_file_path = os.path.join(dir_name, merged_file_name)
     with open(results_file_path, "w", encoding="utf-8") as output_file:
         json.dump(data, output_file)
